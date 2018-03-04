83d8063e3ba3de2cebae5bb344383fd853771598
AndersonCamargo20 <anderson.camargo@unochapeco.edu.br>
2018-03-04 00:37:18 -0800

    Add solutions

--- 1001 - Extremamente Básico.c
+++ 1001 - Extremamente Básico.c
@@ -2,11 +2,11 @@
 
 int main (){
 
-	int A, B, X;
+int A, B, X;
 
-	scanf("%d", &A);
-	scanf("%d", &B);
-	X = A+B;
-	printf("X = %d\n", X);
+scanf("%d", &A);
+scanf("%d", &B);
+X = A+B;
+printf("X = %d\n", X);
 return 0;
 }
